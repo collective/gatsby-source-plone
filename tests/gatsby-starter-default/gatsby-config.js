51e9c7e577f1d3c321fc80da0172b7adb484056e
Andrea Cecchi <andrea.cecchi85@gmail.com>
2017-12-12 14:32:23 +0100

    moved gatsby-node in root and add dependency in test

--- tests/gatsby-starter-default/gatsby-config.js
+++ tests/gatsby-starter-default/gatsby-config.js
@@ -1,6 +1,11 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
+    title: `Gatsby Default Starter`
   },
-  plugins: [`gatsby-plugin-react-helmet`],
-}
+  plugins: [
+    `gatsby-plugin-react-helmet`,
+    {
+      resolve: 'gatsby-source-plone'
+    }
+  ]
+};

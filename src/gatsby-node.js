34ff2356cd03609b6c67bb3f56145da895abf7cc
ajayns <ajay.ns@outlook.com>
2018-05-18 13:40:24 +0530

    Clean up code, add helpers, basic traversal

--- src/gatsby-node.js
+++ src/gatsby-node.js
@@ -1,106 +1,56 @@
-const axios = require(`axios`);
-const crypto = require(`crypto`);
+import axios from "axios";
+import crypto from "crypto";
 
-exports.sourceNodes = async (
-  { boundActionCreators, getNode, hasNodeChanged, store, cache },
-  { baseUrl }
-) => {
-  const { createNode } = boundActionCreators;
+// Helper to create content digest
+const createContentDigest = item =>
+  crypto
+  .createHash(`md5`)
+  .update(JSON.stringify(item))
+  .digest(`hex`);
 
-  const data = await axios.get(`${baseUrl}/@search`, {
+// Helper to get data from url
+const fetchData = async url => {
+  const {
+    data
+  } = await axios.get(url, {
+    headers: {
+      accept: "application/json"
+    },
     params: {
       metadata_fields: '_all',
     },
-    headers: {
-      accept: 'application/json',
-    },
   });
-  data.data.items.map(async item => {
+  return data;
+};
+
+exports.sourceNodes = async ({
+  boundActionCreators,
+  getNode,
+  hasNodeChanged,
+  store,
+  cache
+}, {
+  baseUrl
+}) => {
+  const {
+    createNode
+  } = boundActionCreators;
+
+  const data = await fetchData(`${baseUrl}/@search`);
+
+  data.items.map(item => {
     let node = {
       ...item,
       internal: {
-        type: item['@type'].replace(' ', ''),
-        contentDigest: crypto
-          .createHash(`md5`)
-          .update(JSON.stringify(item))
-          .digest(`hex`),
-        mediaType: 'text/html',
-      },
+        type: item["@type"].replace(" ", ""),
+        contentDigest: createContentDigest(item),
+        mediaType: "text/html"
+      }
     };
-    node.id = item['@id'];
+    node.id = item["@id"];
     node.parent = null;
     node.children = [];
-    console.log(JSON.stringify(node));
-    // console.log(JSON.stringify(data.data.items));
     createNode(node);
     return;
   });
-
-  // const data = await axios.get("http://localhost:8080/Plone/news/aggregator", {
-  //   headers: {
-  //     accept: "application/json"
-  //   }
-  // });
-  // const news = await Promise.all(
-  //   data.data.items.map(async item => {
-  //     const url = item["@id"];
-  //     const getNext = async url => {
-  //       const res = await axios.get(url, {
-  //         headers: {
-  //           accept: "application/json"
-  //         }
-  //       });
-  //       return res.data;
-  //     };
-  //
-  //     const itemDetails = await getNext(url);
-  //
-  //     let node = {
-  //       ...itemDetails,
-  //       slug: `/${itemDetails.id}/`,
-  //       internal: {
-  //         type: "NewsItem",
-  //         contentDigest: crypto
-  //           .createHash(`md5`)
-  //           .update(JSON.stringify(itemDetails))
-  //           .digest(`hex`)
-  //       }
-  //     };
-  //     node.id = itemDetails.UID;
-  //     node.parent = null;
-  //     node.children = [];
-  //     return node;
-  //   })
-  // );
-  // news.map(x => createNode(x));
 };
-
-// exports.createPages = ({ graphql, boundActionCreators }) => {
-//   const { createPage } = boundActionCreators;
-//   return new Promise((resolve, reject) => {
-//     graphql(`
-//       {
-//         allNewsItem {
-//           edges {
-//             node {
-//               id
-//               slug
-//             }
-//           }
-//         }
-//       }
-//     `).then(result => {
-//       result.data.allNewsItem.edges.map(({ node }) => {
-//         createPage({
-//           path: node.slug,
-//           component: path.resolve(`./src/templates/news-item.js`),
-//           context: {
-//             // Data passed to context is available in page queries as GraphQL variables.
-//             id: node.id
-//           }
-//         });
-//       });
-//       resolve();
-//     });
-//   });
-// };

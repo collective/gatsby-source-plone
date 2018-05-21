6d5db480394e413ca5c56d1b36bed4ad48f84cee
Asko Soukka <asko.soukka@iki.fi>
2018-05-21 21:33:35 +0300

    Change example to use bootstrap

--- tests/gatsby-starter-default/gatsby-config.js
+++ tests/gatsby-starter-default/gatsby-config.js
@@ -1,12 +1,13 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`
+    title: `Gatsby Default Starter`,
   },
   plugins: [
     {
       resolve: `gatsby-source-plone`,
       options: { baseUrl: `http://localhost:8080/Plone/` },
     },
-    `gatsby-plugin-react-helmet`
-  ]
+    `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-sass`,
+  ],
 };

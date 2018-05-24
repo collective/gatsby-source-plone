8188d07c5d2544abd4cd9531cfbea546fccfa2d7
Asko Soukka <asko.soukka@iki.fi>
2018-05-24 21:05:15 +0300

    Fix issue where contents listing links were broken with path-prefix

--- tests/gatsby-starter-default/src/pages/index.js
+++ tests/gatsby-starter-default/src/pages/index.js
@@ -18,7 +18,7 @@ export default ({ data }) => {
     } else {
       contents.push(
         <h4>
-          <Link to={node.fields.slug}>{node.title}</Link>
+          <Link to={`/${node.fields.slug}/`}>{node.title}</Link>
         </h4>
       );
     }

8359247d67011d40678e03af4005ccfb220578b8
Asko Soukka <asko.soukka@iki.fi>
2018-06-30 17:29:30 +0300

    Update nix-env

--- shell.nix
+++ shell.nix
@@ -5,7 +5,9 @@ pkgs.stdenv.mkDerivation rec {
   buildInputs = with pkgs; [
     geckodriver
     jq
+    libpng.dev
     nodejs-9_x
+    pngquant
     python3
     travis
     (yarn.override {
@@ -14,5 +16,9 @@ pkgs.stdenv.mkDerivation rec {
   ];
   shellHook = ''
     export SSL_CERT_FILE=${pkgs.cacert}/etc/ssl/certs/ca-bundle.crt
+    if [ -f tests/gatsby-starter-default/node_modules/pngquant-bin/vendor/pngquant ]; then
+      rm tests/gatsby-starter-default/node_modules/pngquant-bin/vendor/pngquant
+      ln -s $(which pngquant) tests/gatsby-starter-default/node_modules/pngquant-bin/vendor/pngquant
+    fi
   '';
 }

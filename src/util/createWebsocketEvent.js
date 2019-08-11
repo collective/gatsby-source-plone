e02186de9f41c6d3f749360dda91f9468c1bf83c
iFlameing <ialokkumarsingh0@gmail.com>
2019-08-12 01:54:40 +0530

    adding file for createWebsocketEvent.js

---
+++ src/util/createWebsocketEvent.js
@@ -0,0 +1,27 @@
+import { ploneNodeGenerator } from './ploneNodeGenerator';
+
+export const createWebsocketEvent = async function(
+  data,
+  token,
+  baseUrl,
+  expansions,
+  backlinks,
+  createNode,
+  reporter
+) {
+  let urlChild = data['created'][0]['@id'];
+  let urlParent = data['created'][0]['parent']['@id'];
+  let urlList = [urlChild, urlParent];
+  for (const url of urlList) {
+    for await (const node of ploneNodeGenerator(
+      url,
+      token,
+      baseUrl,
+      expansions,
+      backlinks
+    )) {
+      reporter.info(`Creating node – ${node.id.replace(baseUrl, '') || '/'}`);
+      createNode(node);
+    }
+  }
+};

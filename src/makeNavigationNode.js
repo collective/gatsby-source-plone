00c860f069e6ebefd2dce4db0b469aca32b2402d
iFlameing <ialokkumarsingh0@gmail.com>
2019-08-12 00:39:54 +0530

    adding separate file for makeNavigationNode.js

---
+++ src/makeNavigationNode.js
@@ -0,0 +1,12 @@
+export const makeNavigationNode = (id, data, path) => {
+  return {
+    ...data,
+    id: id,
+    internal: {
+      contentDigest: createContentDigest(data),
+      mediaType: 'application/json',
+      type: 'PloneNavigation',
+    },
+    _path: path,
+  };
+};
